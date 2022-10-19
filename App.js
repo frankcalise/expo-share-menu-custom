c3b3d5145fa143415c2eebcb0bd1a9dfcb4bdcde
Frank Calise <fcalise@gmail.com>
2022-10-19 10:24:53 -0400

    custom share view controller

--- App.js
+++ App.js
@@ -54,59 +54,6 @@ const App = () => {
   );
 };
 
-const Share = () => {
-  const [sharedData, setSharedData] = useState("");
-  const [sharedMimeType, setSharedMimeType] = useState("");
 
-  useEffect(() => {
-    ShareMenuReactView.data().then(({ mimeType, data }) => {
-      setSharedData(data);
-      setSharedMimeType(mimeType);
-      console.log("from share", { data });
-    });
-  }, []);
-
-  return (
-    <View>
-      <Button
-        title="Dismiss"
-        onPress={() => {
-          ShareMenuReactView.dismissExtension();
-        }}
-      />
-      <Button
-        title="Send"
-        onPress={() => {
-          // Share something before dismissing
-          ShareMenuReactView.dismissExtension();
-        }}
-      />
-      <Button
-        title="Dismiss with Error"
-        onPress={() => {
-          ShareMenuReactView.dismissExtension("Something went wrong!");
-        }}
-      />
-      <Button
-        title="Continue In App"
-        onPress={() => {
-          ShareMenuReactView.continueInApp();
-        }}
-      />
-      <Button
-        title="Continue In App With Extra Data"
-        onPress={() => {
-          ShareMenuReactView.continueInApp({ hello: "from the other side" });
-        }}
-      />
-      {sharedMimeType === "text/plain" && <Text>{sharedData}</Text>}
-      {sharedMimeType.startsWith("image/") && (
-        <Image source={{ uri: sharedData }} />
-      )}
-    </View>
-  );
-};
 
 export default App;
-// AppRegistry.registerComponent("Test", () => Test);
-// AppRegistry.registerComponent("ShareMenuModuleComponent", () => Share);
